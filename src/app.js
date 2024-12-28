8565a8c1d0ec1dee929c05a8abc691e929797b68
Rajveer Pandey <kram35477@gmail.com>
2024-12-29 00:22:58 +0530

    Connect mongoDb atlas

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+
+
+// MongoDb:
+//   - url: mongodb://localhost:27017/
+//   - IP address (103.108.5.210) 
+// UserName: kram35477
+// Password: JpyFexgyWq8BYD2n
+
+
+// mongodb+srv://kram35477:<db_password>@cluster0.v91ei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
